2e02bb975e78c4507a60890e617792d7b8c8f8e5
Alize Leal Pereira <48735281+alizeleal@users.noreply.github.com>
2025-04-09 04:46:46 -0300

    day one project

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,6 @@
-print("Hello, " + input("What is your name?") + "!")
+print("Welcome to band name generator!")
+birthCity = input("In which city were you born?\n")
+
+petName = input("What's your pet name?\n")
+
+print("\nYour band name is: " + birthCity + " " + petName)
