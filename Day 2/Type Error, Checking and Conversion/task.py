277f752bb74a144467fc76760a000954ab12a963
Alize Leal Pereira <48735281+alizeleal@users.noreply.github.com>
2025-04-10 02:03:22 -0300

    day 32 of coding

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,12 @@
-len(12345)
+print(len("12345"))
+# exercise 1
+print(type("abc"))
+print(type(False))
+print(type(1))
+print(type(1.24))
+
+#exercise 2
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
+
+#exercise 4
+
