093b85fcb45f94007d901fcd6d8c5bffbe5bfb24
Alize Leal Pereira <48735281+alizeleal@users.noreply.github.com>
2025-04-22 02:13:53 -0300

    day 11 final

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,58 +1,92 @@
 import random
-
 import art
-print(art.logo)
-
 cards = [2,3,4,5,6,7,8,9,10,10,10,10,11]
-players_hand =[]
-dealers_hand = []
-
-print("Welcome to Blackjack!")
-play = input("Type y to start playing\n")
-sum_player = 0
-sum_dealer = 0
 
 def draw():
+    """Draw a card from the list \n
+    cards = [2,3,4,5,6,7,8,9,10,10,10,10,11]"""
     return random.choice(cards)
 
-# while play == 'y':
-player_draw = draw()
-dealer_draw = draw()
-another = 'y'
+def blackjack():
+    """Blackjack Game"""
+    print(art.logo)
+    print("Welcome to Blackjack!")
+    play = input("Press Enter to start playing\n")
+    players_hand = []
+    dealers_hand = []
+    #first draws
+    players_hand.append(draw())
+    players_hand.append(draw())
+    dealers_hand.append(draw())
 
-dealers_hand.append(dealer_draw)
-players_hand.append(player_draw)
-print(f"You drew {players_hand}")
-print(f"The Dealer drew {dealers_hand}")
-
-while another == 'y':
-    for i in range(len(players_hand)):
-        sum_player += players_hand[i]
-        sum_dealer += dealers_hand[i]
-    if sum_dealer < 21 and sum_dealer < 21:
-        another = input("Want to draw another card? Type y to continue\n")
-    if sum_dealer > 21:
-        print(f"The dealer currently has: {dealers_hand}")
-        print(f"You won! The dealer has reached {sum_dealer} points.")
-        break
-    elif sum_player > 21:
-        print(f"You drew {players_hand}")
-        print(f"You lose! Your score is {sum_player-21} above the limit of 21 points.")
-        break
-    else:
-        sum_player = 0
-        sum_dealer = 0
-        player_draw = draw()
-        players_hand.append(player_draw)
-        for i in range(len(players_hand)):
-            sum_player += players_hand[i]
-        if sum_player > 21:
-            print(f"You drew {players_hand}")
-            print(f"You lose! Your score is {sum_player - 21} above the limit of 21 points.")
-        elif sum_player == 21:
-            print("You've reached 21 points! You won!")
+    # draw first card
+    if players_hand[-1] == 11 and sum(players_hand) > 21:
+        players_hand.remove(11)
+        players_hand.append(1)
+    print(f"Your current cards are: {players_hand}, scoring {sum(players_hand)}")
+    print(f"The Dealer's first card: {dealers_hand}")
+    if sum(players_hand) == 21 and len(players_hand) == 2:
+        print(f"Your cards are: {players_hand}")
+        print("You've hit a Blackjack!")
+        play = input("Type 'y' and press Enter to play again\n")
+        if play == 'y':
+            print("\n" * 50)
+            blackjack()
         else:
-            print(f"You drew {players_hand}")
-        print(f"The dealer currently has: {dealers_hand}")
-        dealer_draw = draw()
-        dealers_hand.append(dealer_draw)
+            return 0
+    another_card = input("Type 'y' to draw another card. Type 'n' to pass.")
+    while another_card == 'y':
+        players_hand.append(draw())
+        if players_hand[-1] == 11 and sum(players_hand) > 21:
+            players_hand.remove(11)
+            players_hand.append(1)
+        print(f"Your current cards are: {players_hand}, scoring {sum(players_hand)}")
+        print(f"The Dealer's first card: {dealers_hand}")
+
+        if sum(players_hand) > 21:
+            print("\n********----------********")
+            print(f"Your final hand: {players_hand}, final score: {sum(players_hand)}")
+            print(f"The Dealer's final hand: {dealers_hand}, final score: {sum(dealers_hand)}")
+            print("\n\n(ㆆ _ ㆆ)\n\n")
+            print("You overdraw. You lose!")
+            print("********----------********\n")
+            players_hand =[]
+            dealers_hand[0] = draw()
+            play = input("Type 'y' and press Enter to play again\n")
+            if play == 'y':
+                print("\n" * 50)
+                blackjack()
+            else:
+                return 0
+        else:
+            another_card = input("\nType 'y' to draw another card. Type 'n' to pass.\n")
+
+    print(f"\nYour final hand: {players_hand}, final score: {sum(players_hand)}")
+    dealers_hand.append(draw())
+    if sum(dealers_hand) < 18:
+        dealers_hand.append(draw())
+    if dealers_hand[-1] == 11 and sum(dealers_hand) > 21:
+        dealers_hand.remove(11)
+        dealers_hand.append(1)
+    print(f"\nThe Dealer's final hand: {dealers_hand}, final score: {sum(dealers_hand)}")
+    if sum(dealers_hand) > 21:
+        print("\n\n	☜(⌒▽⌒)☞\n\n")
+        print("The dealer overdrawn! You win!")
+    elif sum(players_hand) < sum(dealers_hand):
+        print("\n\n(ㆆ _ ㆆ)\n\n")
+        print("You lose!")
+    elif sum(players_hand) > sum(dealers_hand):
+        print("\n\n	☜(⌒▽⌒)☞\n\n")
+        print("You won!")
+    else:
+        print("\n\n	(╥﹏╥)\n\n")
+        print("It's a draw!")
+    print("********----------********")
+    play = input("Type 'y' and press Enter to play again\n")
+    if play == 'y':
+        print("\n"*50)
+        blackjack()
+    else:
+        return 0
+
+blackjack()
